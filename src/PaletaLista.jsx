49b449dacfdc1cae8ca828eb34e736264f20e8a2
alexmeirelles <alemeirelles@msn.com>
2022-04-29 10:48:50 -0300

    feat: removebutton

--- src/PaletaLista.jsx
+++ src/PaletaLista.jsx
@@ -10,22 +10,30 @@ function PaletaLista() {
     const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1};
     setPaletaSelecionada({...paletaSelecionada, ...paleta});
   }
+
+  const removerItem =(paletaIndex) => {
+    const paleta = {[paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) -1};
+    setPaletaSelecionada({...paletaSelecionada, ...paleta});
+  }
+
+  const badgeCounter = (canRender, index) =>
+	Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletaSelecionada[index]} </span>);
+
+  const removeButton = (canRender, index) =>
+	Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>);
+
     return (
       <div className="PaletaLista">
       {paletas.map((paleta, index) =>
         <div className="PaletaListaItem" key= {`PaletaListaItem-${index}`}>
-        	<span className="PaletaListaItem__badge"> {paletaSelecionada[index] || 0} </span>
+          	{badgeCounter(paletaSelecionada[index], index)}
           <div>
               <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
               <div className="PaletaListaItem__preco">{paleta.preco}</div>
               <div className="PaletaListaItem__descricao"> {paleta.descricao}  </div>
               <div className="PaletaListaItem__acoes Acoes">
-                <button 
-                className="Acoes__adicionar Acoes__adicionar--preencher"
-                onClick={() => adicionarItem(index)}
-                >
-                  Adicionar Ao Carrinho
-                </button>
+              <button className={`Acoes__adicionar ${!paletaSelecionada[index] && "Acoes__adicionar--preencher"}`} onClick={() => adicionarItem(index)}>adicionar</button>
+              {removeButton(paletaSelecionada[index], index)}
               </div>
             </div>
             <img className="PaletaListaItem__foto" src={paleta.foto} alt="Paleta de Doce de Leite" />
