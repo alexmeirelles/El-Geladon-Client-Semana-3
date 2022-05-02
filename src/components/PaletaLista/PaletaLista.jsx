e1e31cb21c1a6fd4c5b353ff1a6358d25789b293
alexmeirelles <alemeirelles@msn.com>
2022-05-02 20:38:57 -0300

    feat modal e overlay

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -2,11 +2,26 @@ import "./PaletaLista.css";
 import { useState, useEffect } from "react";
 import PaletaListaItem from "components/PaletaListaItem/PaletaListaItem";
 import { PaletaService } from "services/PaletaService";
+import PaletaDetalhesModal from "components/PaletaDetalhesModal/PaletaDetalhesModal";
+
 
 function PaletaLista() {
   const [paletas, setPaletas] = useState([]);
 
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+  const [paletaModal, setPaletaModal] = useState(false);
+
+  const paleta = {
+    titulo: "Açaí com Leite Condensado",
+    descricao:
+      "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+    foto: "assets/images/acai-com-leite-condensado.png",
+    preco: 10.0,
+    sabor: "Açaí",
+    recheio: "Leite Condensado",
+    possuiRecheio: true,
+  };
 
   const adicionarItem = (paletaIndex) => {
     const paleta = {
@@ -35,14 +50,16 @@ function PaletaLista() {
     <div className="PaletaLista">
       {paletas.map((paleta, index) => (
         <PaletaListaItem
-          key={`PaletaListaItem-${index}`}
-          paleta={paleta}
-          quantidadeSelecionada={paletaSelecionada[index]}
-          index={index}
-          onAdd={(index) => adicionarItem(index)}
-          onRemove={(index) => removerItem(index)}
-        />
+           key={`PaletaListaItem-${index}`}
+           paleta={paleta}
+           quantidadeSelecionada={paletaSelecionada[index]}
+           index={index}
+           onAdd={(index) => adicionarItem(index)}
+      onRemove={(index) => removerItem(index)}
+      clickItem={(paletaId) => setPaletaModal(paleta)}
+           />
       ))}
+     {paletaModal && <PaletaDetalhesModal paleta={paletaModal} closeModal={() => setPaletaModal(false)} />}
     </div>
   );
 }
